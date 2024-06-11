bb25ea633112b8530abc169552fda311b8507094
Kelly Reis Lee <kellyreislee@MacBook-Pro-de-Kelly.local>
2024-06-11 16:11:10 +0100

    Learning ejs(template engine)

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const path = require('path')
 
-app.set('view engine', 'pug')
+
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.use(express.urlencoded({ extended: true }))
@@ -17,7 +18,7 @@ app.use(shopRoutes)
 
 app.use((req, res, next) => {
   //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-  res.status(404).render('404', { pageTitle: 'Page Not Found' })
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '' })
 })
 
 app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const products = [];
 router.get('/add-product', (req, res, next) => {
   console.log('In another middleware')
   //res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'))
-  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' })
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true })
 })
 
 router.post('/add-product', (req, res, next) => {
