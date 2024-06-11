1c42961ed217e45417b149b144cd5edba98256ae
Kelly Reis Lee <kellyreislee@MacBook-Pro-de-Kelly.local>
2024-06-11 10:45:37 +0100

    Adding pug(template engine)

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,19 @@
 const router = require('express').Router()
+const path = require('path');
+const { title } = require('process');
+const products = [];
 
 router.get('/add-product', (req, res, next) => {
   console.log('In another middleware')
-  res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>')
+  res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'))
 })
 
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body.title)
+  products.push({ title: req.body.title })
   res.redirect('/')
 })
 
 
-module.exports = router
+module.exports = {
+  router, products
+}
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
-
+const path = require('path')
+const adminData = require('./admin')
 
 
 router.get('/', (req, res, next) => {
   console.log('One middleware')
-  res.send('<h1>Hello from Express</h1>')
+  console.log('shop.js', adminData.products)
+  res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'))
 })
 
 
