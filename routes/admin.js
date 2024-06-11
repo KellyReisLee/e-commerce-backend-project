139a9ab38bbb4fdea819ba922a08b085e60c4216
Kelly Reis Lee <kellyreislee@MacBook-Pro-de-Kelly.local>
2024-06-11 14:26:37 +0100

    finishing with pug(template engine)

--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,8 @@ const products = [];
 
 router.get('/add-product', (req, res, next) => {
   console.log('In another middleware')
-  res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'))
+  //res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'))
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' })
 })
 
 router.post('/add-product', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,9 @@ const adminData = require('./admin')
 router.get('/', (req, res, next) => {
   console.log('One middleware')
   console.log('shop.js', adminData.products)
-  res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'))
+  const products = adminData.products;
+  // res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'))
+  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' })
 })
 
 
