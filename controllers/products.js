dc463771445a0bf6ba5e000d524057966268f026
Kelly Reis Lee <kellyreislee@MacBook-Pro-de-Kelly.local>
2024-06-13 17:01:46 +0100

    create model using classes in js

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,11 @@
 
-const path = require('path');
-const products = [];
+
+const Product = require('../models/product')
 
 
 //GET - Get all products:
 exports.getAllProducts = async (req, res, next) => {
-  console.log('In another middleware')
+  //console.log('In another middleware')
   //res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'))
   res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true })
 }
@@ -13,12 +13,13 @@ exports.getAllProducts = async (req, res, next) => {
 
 //POST - Adding new Products:
 exports.addProducts = (req, res, next) => {
-  products.push({ title: req.body.title })
+  const product = new Product(req.body.title)
+  product.save();
   res.redirect('/')
 }
 
 exports.getProducts = (req, res, next) => {
-
+  const products = Product.fetchAllData();
   // res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'))
   res.render('shop', {
     prods: products,
