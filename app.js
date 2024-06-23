0123304c4ff93eec1984b736dce635ee0f8a2f2e
Kelly Reis Lee <kellyreislee@MacBook-Pro-de-Kelly.local>
2024-06-23 18:37:41 +0100

    Adding mySQL database

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const path = require('path')
 const errorController = require('./controllers/404')
-
+const db = require('./util/database')
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
+
+// db.execute('SELECT * FROM products').then((result) => console.log(result[0], result[1])).catch((err) => console.log(err))
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
--- util/database.js
+++ util/database.js
@@ -1,1 +1,11 @@
 const mysql = require('mysql2')
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'node_complete',
+  password: 'casa1324',
+  port: 3306
+})
+
+module.exports = pool.promise();
