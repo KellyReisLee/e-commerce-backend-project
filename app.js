23619fa578a0a329fe4d97d2dc83bb6f26b66b29
Kelly Reis Lee <kellyreislee@MacBook-Pro-de-Kelly.local>
2024-06-23 22:22:56 +0100

    adding dotenv

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+require('dotenv').config()
 const express = require('express')
 const app = express();
 const adminRoutes = require('./routes/admin')
@@ -6,13 +7,15 @@ const shopRoutes = require('./routes/shop')
 const path = require('path')
 const errorController = require('./controllers/404')
 const db = require('./util/database')
+
+
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-// db.execute('SELECT * FROM products').then((result) => console.log(result[0], result[1])).catch((err) => console.log(err))
+db.execute('SELECT * FROM products').then((result) => console.log(result[0], result[1])).catch((err) => console.log(err))
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   database: 'node_complete',
-  password: 'casa1324',
+  password: process.env.DATABASE_PASSWORD,
   port: 3306
 })
 
